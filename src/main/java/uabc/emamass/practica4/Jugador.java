5ad30fe9ae239fba7b3fb1c7722d5f009850d3e5
EmaMass <emanuel.massuda@uabc.edu.mx>
2024-04-19 10:59:01 -0700

    Falta Codigo GUI Hecho

---
+++ src/main/java/uabc/emamass/practica4/Jugador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uabc.emamass.practica4;
+
+/**
+ *
+ * @author Emanuel Seiji Massuda Cuevas (poppe)
+ */
+public class Jugador {
+    
+}
---
+++ src/main/java/uabc/emamass/practica4/Practica4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uabc.emamass.practica4;
+
+/**
+ *
+ * @author Emanuel Seiji Massuda Cuevas (poppe)
+ */
+public class Practica4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
